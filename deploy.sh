87d8b0f7dad71556a2937801102ff7ee0a4a4690
github-young <xymailmooc@163.com>
2019-05-26 23:29:25 +0800

    fix transparent bg bug

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 rm -rf docs/
-../../hugo
+cd ../../
+hugo
+cd themes/hugo-theme-next/
 cp -r ../../public/ ./docs
+git status
 git add docs/
 git commit -m "update docs for preview $(date)"
 git push -u origin master
